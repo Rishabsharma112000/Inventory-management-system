4a2820ffb47d0debab09e6376a29984083c85d23
Rishab Sharma <rishabsharma112000@gmail.com.com>
2026-06-19 21:14:23 +0530

    debug api url

--- ims-frontend/src/services/api.js
+++ ims-frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
+const url = `${import.meta.env.VITE_API_URL}/api/auth/register`;
+console.log("FINAL URL:", url);
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api',
