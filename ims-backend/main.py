dfba83002714c3e3ecdd8caaf7e661e052d0eb5a
Rishab Sharma <rishabsharma112000@gmail.com.com>
2026-06-19 20:44:15 +0530

    Update cors

--- ims-backend/main.py
+++ ims-backend/main.py
@@ -29,7 +29,10 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://inventory-management-system-c5108tzus-rishab-sharma-s-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
